ca53feba8f31a5366f97558d3e2b3b562d21afd3
luk4sW <lukas.wildeisen@stud.kftg.ch>
2022-12-23 09:17:54 +0100

    Start

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta content="Lukas Wildeisen" name="author">
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+    <meta content="Bootstrap V4.3.1 Template für IMS Frauenfeld" name="description">
+    <link href="css/css.css" rel="stylesheet">
+    <link href="js/js.js" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+    <title>Nutzungsflächen Thurgau</title>
+</head>
+
+<body>
+
+    <h1 id="titel">Nutzungsflächen Thurgau</h1>
+    <img src="img/ktTGWappen.png" alt="Wappen des Kanton Thurgau" id="titleimage">
+    <h2 id="info">Vorwort</h2>
+    <p id="vorwort">Diese Website wurde für das Modul 306 erstellt. Es geht darum in der Gruppe ein kleines Projekt zu erstellen.
+        Die Verantwortung über dieses Projekt lag bei den jeweiligen 4er oder 5er Gruppen.
+        Das Ziel dieser Webseite ist es die Nutzungsflächen des Kantons Thurgau darzustellen.
+        Die Daten beinhaltet die landwirtschaftlich genutzten Flächen gemäss landwirtschaftlicher Begriffsverordnung (LBV) und Direktzahlungsverordnung (DZV).
+        Sie werden von dem Amt für Geoinformationen des Kanton Thurgau kostenlos zur verfügung gestellt.
+    </p>
+    <br>
+    <br>
+    <h2 id="info">Karte</h2>
+    <div id="map"></div>
+    <br>
+    <br>
+
+
+
+    <script type="text/javascript" src="js/myjs.js" charset="UTF-8"></script>
+    <script src="js/js.js"></script>
+</body>
+<footer>
+    <p id="fo_infos">Diese Seite wurde von ©<a href="https://github.com/luw-i">Lukas Wildeisen</a> erstellt. Dies Geschah unter der Leitung von <a href="https://github.com/jpmou">Jean-Pierre Mouret</a>.
+        Weiter wurde das Projekt mit der Hilfe von dem Repository <a href="https://github.com/Yeshush/m306-OGD">Yeshush/m306-OGD</a> erstellt. Es wurde Als Informationsquelle für das Auslesen von WMS-Daten verwendet.
+        Es wurde die Daten von <a href="https://opendata.swiss/de/dataset/nutzungsflachen">opendata.swiss
+        </a> verwendet.
+    </p>
+    <ul>
+        <li>Source-Code<a href="">>Github</a></li>
+        <li>Datenquelle<a href="https://opendata.swiss/de/dataset/nutzungsflachen">opendata.swiss</a></li>
+        <li>Leaflet JS<a href="https://leafletjs.com/"> Leaflet.js</a></li>
+        <li>Hilfsrepository von Yeshush<a href="https://github.com/Yeshush/m306-OGD">Github-Repository
+            </a></li>
+    </ul>
+</footer>
+
+</html>
---
+++ js/js.js
@@ -0,0 +1,158 @@
+/* Global L -> Leaflet JS */
+
+const Attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileURL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+const map = L.map("map", {
+  center: [47.577331, 9.000088],
+  zoom: 7,
+  minZoom: 10,
+  //Wenn über diese Koordinaten hinweg, dan wird die Karte nicht zurückverschoben verschoben.
+  maxBounds: [
+    //Südwestlich
+    [47.314953, 8.536811],
+    //Nordöstlich
+    [47.760957, 9.60961],
+  ],
+});
+
+//Ein Layer wird hinzugefügt
+L.tileLayer(tileURL, { Attribution }).addTo(map);
+//Layer Nutzflächen, Transparent Ja und Nein
+const nutzflaechen = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: true,
+    format: "image/png",
+    layers: "Nutzungsflaechen",
+    crs: L.CRS.EPSG4326,
+  }
+);
+const nutzflaechenT = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: false,
+    format: "image/png",
+    layers: "Nutzungsflaechen",
+    crs: L.CRS.EPSG4326,
+  }
+);
+//Layer Nutzungsflächen_Hauptkulturen_TG, Transparent Ja und Nein
+const nutzungsflaechen_flaechen_tg = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: true,
+    format: "image/png",
+    layers: "nutzungsflaechen_flaechen_tg",
+    crs: L.CRS.EPSG4326,
+  }
+);
+const nutzungsflaechen_flaechen_tgT = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: false,
+    format: "image/png",
+    layers: "nutzungsflaechen_flaechen_tg",
+    crs: L.CRS.EPSG4326,
+  }
+);
+//Layer Nutzungsflächen, Transparent Ja und Nein
+const nutzungsflaechen_hauptkulturen = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: true,
+    format: "image/png",
+    layers: "nutzungsflaechen_hauptkulturen",
+    crs: L.CRS.EPSG4326,
+  }
+);
+const nutzungsflaechen_hauptkulturenT = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: false,
+    format: "image/png",
+    layers: "nutzungsflaechen_hauptkulturen",
+    crs: L.CRS.EPSG4326,
+  }
+);
+//Layer nutzungsflaechen_, Transparent Ja und Nein
+const nutzungsflaechen_ = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: true,
+    format: "image/png",
+    layers: "nutzungsflaechen_",
+    crs: L.CRS.EPSG4326,
+  }
+);
+const nutzungsflaechen_T = L.tileLayer.wms(
+  "https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?",
+  {
+    version: "1.3.0",
+    transparent: false,
+    format: "image/png",
+    layers: "nutzungsflaechen_",
+    crs: L.CRS.EPSG4326,
+  }
+);
+//Ein Array der Layer wird erstellt
+const layers = {
+  "Nutzungsflächen (Hauptkategorien Textur)": nutzflaechen,
+  "Nutzungsflächen (Hauptkategorien, Transparent)": nutzflaechenT,
+  "Nutzungsflächen (BFF QI Fläche TG)": nutzungsflaechen_flaechen_tg,
+  "Nutzungsflächen (BFF QI Fläche TG, Transparent)":
+    nutzungsflaechen_flaechen_tgT,
+  "Nutzungsflächen (Hauptkategorien)": nutzungsflaechen_hauptkulturen,
+  "Nutzungsflächen (Hauptkategorien, Transparent)":
+    nutzungsflaechen_hauptkulturenT,
+  Nutzungsflächen: nutzungsflaechen_,
+  "Nutzungsflächen, Transparent": nutzungsflaechen_T,
+};
+
+//Hinzufügen eines Layer-Controller
+L.control.layers(layers).addTo(map);
+
+//Maßstabsbalken zur Karte hinzufügen
+L.control
+  .scale({
+    metric: true,
+    imperial: true,
+  })
+  .addTo(map)
+  .addTo(map);
+
+//Ausgewählten Layer speichern
+var currentLayername;
+//Legende hnzufügen
+const legende = L.control({ position: "bottomright" });
+map.on("baselayerchange", function (e) {
+  currentLayername = e.name;
+  legende.onAdd = function (map) {
+    var div = L.DomUtil.create("div", "Legende");
+    if (
+      currentLayername.valueOf() === "Nutzungsflächen (Hauptkategorien Textur)"
+    ) {
+      div.innerHTML = '<img src="img/nutzflaechen.png">';
+    } else if (
+      currentLayername.valueOf() === "Nutzungsflächen (BFF QI Fläche TG)"
+    ) {
+      div.innerHTML = '<img src="img/nutzungsflaechen_flaechen_tg.png">';
+    } else if (
+      currentLayername.valueOf() === "Nutzungsflächen (Hauptkategorien)"
+    ) {
+      div.innerHTML = '<img src="img/nutzungsflaechen_hauptkulturen.png">';
+    } else if (currentLayername.valueOf() === "Nutzungsflächen") {
+      div.innerHTML = '<img src="img/nutzungsflaechen_.png">';
+    }
+    return div;
+  };
+  legende.addTo(map);
+});
